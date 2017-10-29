a247dad1e51734a6736efd73c37daee0dd04e48e
damageekova <32967380+damageekova@users.noreply.github.com>
2017-10-29 21:28:17 +0300

    Update секонд2.cpp

--- секонд2.cpp
+++ секонд2.cpp
@@ -8,13 +8,13 @@ int k;
 double pow(double a, int k)
  {
     if(k == 0) return 1;
-    if(k < 0) return pow ( 1/a, -k);
-    if(k%2==1) return a * pow (a, k-1);
+    if(k < 0) return pow ( 1/a, - k);
+    if(k%2 == 1) return a * pow (a, k - 1);
     else return pow(a*a, k/2);
  }
 int main (){
-  cin>>a>>k;
-  cout<<pow(a,k);
+  cin >> a >> k;
+  cout << pow(a,k);
   return 0;
 }
 
