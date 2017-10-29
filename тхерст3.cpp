2df13dd94940924946f53377ff55d57d8660656f
damageekova <32967380+damageekova@users.noreply.github.com>
2017-10-29 21:29:40 +0300

    Update тхерст3.cpp

--- тхерст3.cpp
+++ тхерст3.cpp
@@ -4,17 +4,19 @@ using namespace std;
 либо на 72 либо на 27(не вместе).*/
 int main (){
 int n,s;
-s=0;
+s = 0;
 int A[n];
-cin>>n;
- for (int i=0; i<n;i++){
-      cin>>A[i];
+cin >> n;
+ for (int i = 0; i < n; i++){
+      cin >> A[i];
 }
 for (int i=0;i<n;i++){
-if (A[i]%27==0){if (A[i]%72!=0) s=s+A[i];};
-if (A[i]%72==0){if (A[i]%27!=0) s=s+A[i];};
+     if (A[i]%27 == 0) {if (A[i]%72 != 0) s = s + A[i]; 
+                       };
+     if (A[i]%72 == 0) {if (A[i]%27 != 0) s = s + A[i];
+                       };
 }
-cout<<s;
+cout << s;
 return 0;
 }
 
